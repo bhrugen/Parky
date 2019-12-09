cf1c35418ac2cad23eb4cabaf329811ab31bc5a8
Ben Spark <bhrugen1990@gmail.com>
2019-12-09 08:59:53 -0600

    4 NationalParkDto.tscproj

---
+++ ParkyAPI/Models/Dtos/NationalParkDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyAPI.Models.Dtos
+{
+    public class NationalParkDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string State { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Established { get; set; }
+    }
+}
