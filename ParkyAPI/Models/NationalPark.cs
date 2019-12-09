1ba560ca19d68f7fd2b394962df39fa901c3c435
Ben Spark <bhrugen1990@gmail.com>
2019-12-09 07:59:55 -0600

    Add project files.

---
+++ ParkyAPI/Models/NationalPark.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyAPI.Models
+{
+    public class NationalPark
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string State { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Established { get; set; }
+    }
+}
