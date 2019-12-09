0a0511fb30a84972721a9adf7677408579bb804a
Ben Spark <bhrugen1990@gmail.com>
2019-12-09 08:11:48 -0600

    Section 2 - Setup Database Connection

---
+++ ParkyAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ParkyAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<NationalPark> NationalParks { get; set; }
+    }
+}
