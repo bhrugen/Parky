474534141fffb88aa76ec7c626b8d5aca695e4f5
Ben Spark <bhrugen1990@gmail.com>
2019-12-09 13:35:02 -0600

    Section 5 - Adding Swashbuckle to API

--- ParkyAPI/Startup.cs
+++ ParkyAPI/Startup.cs
@@ -36,6 +36,14 @@ namespace ParkyAPI
 
             services.AddScoped<INationalParkRepository, NationalParkRepository>();
             services.AddAutoMapper(typeof(ParkyMappings));
+            services.AddSwaggerGen(options=> {
+                options.SwaggerDoc("ParkyOpenAPISpec",
+                    new Microsoft.OpenApi.Models.OpenApiInfo()
+                    {
+                        Title = "Parky API",
+                        Version = "1"
+                    });
+            });
             services.AddControllers();
         }
 
@@ -48,7 +56,7 @@ namespace ParkyAPI
             }
 
             app.UseHttpsRedirection();
-
+            app.UseSwagger();
             app.UseRouting();
 
             app.UseAuthorization();
