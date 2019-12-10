1fdee817efb6e61ffcbe0f5b8801965c9aefee0f
Ben Spark <bhrugen1990@gmail.com>
2019-12-10 06:56:49 -0600

    Section 6 - Trail Model and Trail Dto

---
+++ ParkyAPI/Models/Dtos/TrailDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using static ParkyAPI.Models.Trail;
+
+namespace ParkyAPI.Models.Dtos
+{
+    public class TrailDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public double Distance { get; set; }
+
+        public DifficultyType Difficulty { get; set; }
+        [Required]
+        public int NationalParkId { get; set; }
+
+        public NationalParkDto NationalPark { get; set; }
+    }
+}
---
+++ ParkyAPI/Models/Trail.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyAPI.Models
+{
+    public class Trail
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public double Distance { get; set; }
+
+        public enum DifficultyType { Easy, Moderate, Difficult, Expert}
+
+        public DifficultyType Difficulty { get; set; }
+        [Required]
+        public int NationalParkId { get; set; }
+
+        [ForeignKey("NationalParkId")]
+        public NationalPark NationalPark { get; set; }
+
+        public DateTime DateCreated { get; set; }
+    }
+}
