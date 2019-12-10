a06adf6b6d5c8c326e0cab7a1f7922c01afa0a3f
Ben Spark <bhrugen1990@gmail.com>
2019-12-10 10:53:46 -0600

    Section 8 - Runtime Compilation and HttpClient

--- ParkyWeb/Startup.cs
+++ ParkyWeb/Startup.cs
@@ -23,7 +23,8 @@ namespace ParkyWeb
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
+            services.AddHttpClient();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
