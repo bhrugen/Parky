2b9ef3a7484e245aca88c5cdf5b14852858ced89
Ben Spark <bhrugen1990@gmail.com>
2019-12-10 15:12:30 -0600

    Section 9 - Upsert Post Action

--- ParkyWeb/Controllers/NationalParksController.cs
+++ ParkyWeb/Controllers/NationalParksController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -40,6 +41,47 @@ namespace ParkyWeb.Controllers
                 return NotFound();
             }
             return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upsert(NationalPark obj)
+        {
+            if (ModelState.IsValid)
+            {
+                var files = HttpContext.Request.Form.Files;
+                if (files.Count > 0)
+                {
+                    byte[] p1 = null;
+                    using (var fs1 = files[0].OpenReadStream())
+                    {
+                        using (var ms1 = new MemoryStream())
+                        {
+                            fs1.CopyTo(ms1);
+                            p1 = ms1.ToArray();
+                        }
+                    }
+                    obj.Picture = p1;
+                }
+                else
+                {
+                    var objFromDb = await _npRepo.GetAsync(SD.NationalParkAPIPath, obj.Id);
+                    obj.Picture = objFromDb.Picture;
+                }
+                if (obj.Id == 0)
+                {
+                    await _npRepo.CreateAsync(SD.NationalParkAPIPath, obj);
+                }
+                else
+                {
+                    await _npRepo.UpdateAsync(SD.NationalParkAPIPath+obj.Id, obj);
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return View(obj);
+            }
         }
 
         public async Task<IActionResult> GetAllNationalPark()
