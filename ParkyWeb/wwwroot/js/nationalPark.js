0cbd34ca2106f7168708072368edc2889eac2351
Ben Spark <bhrugen1990@gmail.com>
2019-12-10 14:01:14 -0600

    Section 9 - Javascript to Load Data Table

---
+++ ParkyWeb/wwwroot/js/nationalPark.js
@@ -0,0 +1,32 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            "url": "/nationalParks/GetAllNationalPark",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "name", "width": "50%" },
+            { "data": "state", "width": "20%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `<div class="text-center">
+                                <a href="/nationalParks/Upsert/${data}" class='btn btn-success text-white'
+                                    style='cursor:pointer;'> <i class='far fa-edit'></i></a>
+                                    &nbsp;
+                                <a onclick=Delete("/nationalParks/Delete/${data}") class='btn btn-danger text-white'
+                                    style='cursor:pointer;'> <i class='far fa-trash-alt'></i></a>
+                                </div>
+                            `;
+                }, "width": "30%"
+            }
+        ]
+    });
+}
