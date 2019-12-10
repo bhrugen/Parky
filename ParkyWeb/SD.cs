4fdd1c1173164c693d03de7c694a7d1658af2687
Ben Spark <bhrugen1990@gmail.com>
2019-12-10 10:49:20 -0600

    Section 8 - API Static Details

---
+++ ParkyWeb/SD.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyWeb
+{
+    public static class SD
+    {
+        public static string APIBaseUrl = "https://localhost:44346/";
+        public static string NationalParkAPIPath = APIBaseUrl+ "api/v1/nationalparks";
+        public static string TrailAPIPath = APIBaseUrl+ "api/v1/trails";
+
+    }
+}
