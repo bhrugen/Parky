f32e5fb53220e58af2d44e98f8389241f23161f5
Ben Spark <bhrugen1990@gmail.com>
2019-12-10 13:20:14 -0600

    Section 8 - Models for Website

---
+++ ParkyWeb/Models/NationalPark.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyWeb.Models
+{
+    public class NationalPark
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string State { get; set; }
+        public byte[] Picture { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Established { get; set; }
+    }
+}
---
+++ ParkyWeb/Models/Trail.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyWeb.Models
+{
+    public class Trail
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public double Distance { get; set; }
+        public enum DifficultyType { Easy, Moderate, Difficult, Expert }
+
+        public DifficultyType Difficulty { get; set; }
+        [Required]
+        public int NationalParkId { get; set; }
+
+        public NationalPark NationalPark { get; set; }
+    }
+}
