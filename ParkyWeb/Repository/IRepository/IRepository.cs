ac275324484cd60ab278423e8b0d2e6096e12987
Ben Spark <bhrugen1990@gmail.com>
2019-12-10 10:59:53 -0600

    Section 8 - Repository Interface

---
+++ ParkyWeb/Repository/IRepository/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyWeb.Repository.IRepository
+{
+    interface IRepository<T> where T : class
+    {
+        Task<T> GetAsync(string url, int Id);
+        Task<IEnumerable<T>> GetAllAsync(string url);
+        Task<bool> CreateAsync(string url, T objToCreate);
+        Task<bool> UpdateAsync(string url, T objToUpdate);
+        Task<bool> DeleteAsync(string url, int Id);
+    }
+}
