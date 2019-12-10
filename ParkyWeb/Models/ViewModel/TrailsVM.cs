c767730b07b6654366bc86317b7330885a26493d
Ben Spark <bhrugen1990@gmail.com>
2019-12-10 15:40:03 -0600

    Section 10 - Trails View Model

---
+++ ParkyWeb/Models/ViewModel/TrailsVM.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyWeb.Models.ViewModel
+{
+    public class TrailsVM
+    {
+        public IEnumerable<SelectListItem> NationalParkList { get; set; }
+        public Trail Trail { get; set; }
+    }
+}
