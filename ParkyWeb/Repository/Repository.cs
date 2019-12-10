e740d900b0c7e50022a2618ee15a7a1b2ef30db8
Ben Spark <bhrugen1990@gmail.com>
2019-12-10 11:29:09 -0600

    Section 8 - CreateAsync in Repository

---
+++ ParkyWeb/Repository/Repository.cs
@@ -0,0 +1,66 @@
+﻿using Newtonsoft.Json;
+using ParkyWeb.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkyWeb.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly IHttpClientFactory _clientFactory;
+
+        public Repository(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+
+        public async Task<bool> CreateAsync(string url, T objToCreate)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Post, url);
+            if(objToCreate!=null)
+            {
+                request.Content = new StringContent(
+                    JsonConvert.SerializeObject(objToCreate), Encoding.UTF8, "application/json");
+            }
+            else
+            {
+                return false;
+            }
+
+            var client = _clientFactory.CreateClient();
+            HttpResponseMessage response = await client.SendAsync(request);
+            if (response.StatusCode == System.Net.HttpStatusCode.Created)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public Task<bool> DeleteAsync(string url, int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetAllAsync(string url)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetAsync(string url, int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateAsync(string url, T objToUpdate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
