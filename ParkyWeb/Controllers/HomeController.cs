2bdecf306de5933b024e32d1655945006861c063
Ben Spark <bhrugen1990@gmail.com>
2019-12-11 08:48:10 -0600

    Section 11 - Index View Model and Home Controller

--- ParkyWeb/Controllers/HomeController.cs
+++ ParkyWeb/Controllers/HomeController.cs
@@ -6,21 +6,32 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using ParkyWeb.Models;
+using ParkyWeb.Models.ViewModel;
+using ParkyWeb.Repository.IRepository;
 
 namespace ParkyWeb.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly INationalParkRepository _npRepo;
+        private readonly ITrailRepository _trailRepo;
+        public HomeController(ILogger<HomeController> logger, INationalParkRepository npRepo,
+            ITrailRepository trailRepo)
         {
+            _npRepo = npRepo;
+            _trailRepo = trailRepo;
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            IndexVM listOfParksAndTrails = new IndexVM()
+            {
+                NationalParkList = await _npRepo.GetAllAsync(SD.NationalParkAPIPath),
+                TrailList = await _trailRepo.GetAllAsync(SD.TrailAPIPath),
+            };
+            return View(listOfParksAndTrails);
         }
 
         public IActionResult Privacy()
---
+++ ParkyWeb/Models/ViewModel/IndexVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyWeb.Models.ViewModel
+{
+    public class IndexVM
+    {
+        public IEnumerable<NationalPark> NationalParkList { get; set; }
+        public IEnumerable<Trail> TrailList { get; set; }
+    }
+}
