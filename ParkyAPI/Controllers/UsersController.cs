4264c5966bf5a1e7f6164f1b5cc56e1940178222
Ben Spark <bhrugen1990@gmail.com>
2020-01-28 15:38:06 -0600

    Section 12 - Authentication Model

--- ParkyAPI/Controllers/UsersController.cs
+++ ParkyAPI/Controllers/UsersController.cs
@@ -24,7 +24,7 @@ namespace ParkyAPI.Controllers
 
         [AllowAnonymous]
         [HttpPost("authenticate")]
-        public IActionResult Authenticate([FromBody] User model)
+        public IActionResult Authenticate([FromBody] AuthenticationModel model)
         {
             var user = _userRepo.Authenticate(model.Username, model.Password);
             if (user == null)
@@ -36,7 +36,7 @@ namespace ParkyAPI.Controllers
 
         [AllowAnonymous]
         [HttpPost("register")]
-        public IActionResult Register([FromBody] User model)
+        public IActionResult Register([FromBody] AuthenticationModel model)
         {
             bool ifUserNameUnique = _userRepo.IsUniqueUser(model.Username);
             if (!ifUserNameUnique)
---
+++ ParkyAPI/Models/AuthenticationModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyAPI.Models
+{
+    public class AuthenticationModel
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
