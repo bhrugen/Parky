409bad9c21fb7ba0dfc547546b4ad4722aca665e
Ben Spark <bhrugen1990@gmail.com>
2020-01-28 15:52:20 -0600

    Section 13 - Add User Model and Account Repositroy

---
+++ ParkyWeb/Repository/AccountRepository.cs
@@ -0,0 +1,30 @@
+﻿using ParkyWeb.Models;
+using ParkyWeb.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ParkyWeb.Repository
+{
+    public class AccountRepository : Repository<User>, IAccountRepository
+    {
+        private readonly IHttpClientFactory _clientFactory;
+
+    public AccountRepository(IHttpClientFactory clientFactory) : base(clientFactory)
+    {
+        _clientFactory = clientFactory;
+    }
+
+        public Task<User> LoginAsync(string url, User objToCreate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RegisterAsync(string url, User objToCreate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ParkyWeb/Repository/IRepository/IAccountRepository.cs
@@ -0,0 +1,15 @@
+﻿using ParkyWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyWeb.Repository.IRepository
+{
+    public interface IAccountRepository : IRepository<User>
+    {
+        Task<User> LoginAsync(string url, User objToCreate);
+        Task<bool> RegisterAsync(string url, User objToCreate);
+
+    }
+}
