75a6353ca824b7c440517953a30da27b3093e184
Ben Spark <bhrugen1990@gmail.com>
2020-01-28 14:45:16 -0600

    Section 12 - Create Token on Successful Authentication

--- ParkyAPI/Repository/UserRepository.cs
+++ ParkyAPI/Repository/UserRepository.cs
@@ -1,9 +1,14 @@
-﻿using ParkyAPI.Data;
+﻿using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using ParkyAPI.Data;
 using ParkyAPI.Models;
 using ParkyAPI.Repository.IRepository;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace ParkyAPI.Repository
@@ -11,16 +16,41 @@ namespace ParkyAPI.Repository
     public class UserRepository : IUserRepository
     {
         private readonly ApplicationDbContext _db;
+        private readonly AppSettings _appSettings;
 
-        public UserRepository(ApplicationDbContext db)
+        public UserRepository(ApplicationDbContext db, IOptions<AppSettings> appsettings)
         {
             _db = db;
-
+            _appSettings = appsettings.Value;
         }
 
         public User Authenticate(string username, string password)
         {
-            throw new NotImplementedException();
+            var user = _db.Users.SingleOrDefault(x => x.Username == username && x.Password == password);
+
+            //user not found
+            if (user == null)
+            {
+                return null;
+            }
+
+            //if user was found generate JWT Token
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[] {
+                    new Claim(ClaimTypes.Name, user.Id.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials
+                                (new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            user.Token = tokenHandler.WriteToken(token);
+
+            return user;
         }
 
         public bool IsUniqueUser(string username)
