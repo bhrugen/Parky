e6af99eec25039d09cf60eef520b21880295b339
Ben Spark <bhrugen1990@gmail.com>
2020-01-28 15:57:07 -0600

    Section 13 - Implement Login and Register in Account Repository

--- ParkyWeb/Repository/AccountRepository.cs
+++ ParkyWeb/Repository/AccountRepository.cs
@@ -1,9 +1,11 @@
-﻿using ParkyWeb.Models;
+﻿using Newtonsoft.Json;
+using ParkyWeb.Models;
 using ParkyWeb.Repository.IRepository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace ParkyWeb.Repository
@@ -17,14 +19,55 @@ namespace ParkyWeb.Repository
         _clientFactory = clientFactory;
     }
 
-        public Task<User> LoginAsync(string url, User objToCreate)
+        public async Task<User> LoginAsync(string url, User objToCreate)
         {
-            throw new NotImplementedException();
+            var request = new HttpRequestMessage(HttpMethod.Post, url);
+            if (objToCreate != null)
+            {
+                request.Content = new StringContent(
+                    JsonConvert.SerializeObject(objToCreate), Encoding.UTF8, "application/json");
+            }
+            else
+            {
+                return new User();
+            }
+
+            var client = _clientFactory.CreateClient();
+            HttpResponseMessage response = await client.SendAsync(request);
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                var jsonString = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<User>(jsonString);
+            }
+            else
+            {
+                return new User() ;
+            }
         }
 
-        public Task<bool> RegisterAsync(string url, User objToCreate)
+        public async Task<bool> RegisterAsync(string url, User objToCreate)
         {
-            throw new NotImplementedException();
+            var request = new HttpRequestMessage(HttpMethod.Post, url);
+            if (objToCreate != null)
+            {
+                request.Content = new StringContent(
+                    JsonConvert.SerializeObject(objToCreate), Encoding.UTF8, "application/json");
+            }
+            else
+            {
+                return false;
+            }
+
+            var client = _clientFactory.CreateClient();
+            HttpResponseMessage response = await client.SendAsync(request);
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
