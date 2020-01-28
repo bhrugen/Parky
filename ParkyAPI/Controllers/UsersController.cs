cef1a641ce9e98f8fb47f971372fb08da1a43fe0
Ben Spark <bhrugen1990@gmail.com>
2020-01-28 14:52:00 -0600

    Section 12 - Add Authenticate Method

---
+++ ParkyAPI/Controllers/UsersController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ParkyAPI.Models;
+using ParkyAPI.Repository.IRepository;
+
+namespace ParkyAPI.Controllers
+{
+    [Authorize]
+    [Route("api/v{version:apiVersion}/Users")]
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserRepository _userRepo;
+        public UsersController(IUserRepository userRepo)
+        {
+            _userRepo = userRepo;
+        }
+
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody] User model)
+        {
+            var user = _userRepo.Authenticate(model.Username, model.Password);
+            if (user == null)
+            {
+                return BadRequest(new { message = "Username or password is incorrect" });
+            }
+            return Ok(user);
+        }
+
+    }
+}
--- ParkyAPI/Repository/IRepository/IUserRepository.cs
+++ ParkyAPI/Repository/IRepository/IUserRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ParkyAPI.Repository.IRepository
 {
-    interface IUserRepository
+    public interface IUserRepository
     {
         bool IsUniqueUser(string username);
         User Authenticate(string username, string password);
